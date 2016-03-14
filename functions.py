a5d6362eeab3e94f88f27f3caa1704810f837405
clay3075 <cjreddick96@gmail.com>
2016-03-13 21:09:40 -0500

    Added more basic interface to GUI. Almost done with GUI. Added database as well.

--- functions.py
+++ functions.py
@@ -1,16 +1,23 @@
 import tkinter as tk
+import sqlite3
 
 
 class Window(tk.Frame):
 	'''Control UI and flow of UI components'''
 	def create_window(self, parent):
 		'''Create basic outline of window'''
-		self.entries = []
 		self.parent = parent
+		self.accountButtonFrame = tk.Frame(self.parent, width=400)
+		self.loadTimes = 0
+		self.db = sqlite3.connect('.accounts.db')
+		self.db.execute('create table if not exists accounts (nickname TEXT, url TEXT, username TEXT, password TEXT);')
+		#self.dbcursor = self.db.cursor()
+		self.entries = []
+		self.loadEntries()
 		self.parent.title("AccountBot: Web")
 		self.parent.geometry("650x550+300+300")
 		self.addButton()
-		#self.parent.grid()
+		self.deleteButton()
 		self.parent.rowconfigure('all', minsize = 200)
 		self.parent.columnconfigure('all', minsize = 200)
 
@@ -19,7 +26,6 @@ class Window(tk.Frame):
 		   if entries to exist show add button top right hand corner.
 		'''
 		self.addB = tk.Button(self.parent, text="Add", height=30, width= 20, command=self.addButtonClicked)
-		#self.addB.grid(row=0, column=0, sticky=tk.E)
 		if (not self.entriesExist()):
 			self.addB.place(relx=0.5, rely=0.5, anchor='center')
 
@@ -27,7 +33,7 @@ class Window(tk.Frame):
 		'''Bring up window to add entry and add entry internally as well'''
 		print('clicked')
 		if (self.entriesExist()):
-			self.addB.place(relx=.8, rely=.1)
+			self.addB.place(relx=.8, rely=.1, anchor='center')
 		self.addItem()
 
 
@@ -36,7 +42,6 @@ class Window(tk.Frame):
 		#create labels
 		self.addB.place_forget()
 		self.inputBox = tk.Frame(self.parent, width=400, height=400)
-		#self.grid()
 		self.inputBox.place(relx=.5,rely=.3, anchor='center')
 		self.nicknameLabel = tk.Label(self.inputBox, text="Nickname: ")
 		self.urlLabel      = tk.Label(self.inputBox, text="URL:      ")
@@ -66,9 +71,13 @@ class Window(tk.Frame):
 		if (self.nicknameEntry.get() and self.urlEntry.get() and self.usernameEntry.get() and self.passwordEntry.get()):
 			self.inputBox.place_forget()
 
+			self.db.execute("INSERT INTO accounts (nickname, url, username, password) \
+							   VALUES (?,?,?,?)",(self.nicknameEntry.get(),self.urlEntry.get(),self.usernameEntry.get(),self.passwordEntry.get()))
+			self.loadAccountButton(self.nicknameEntry.get(),self.urlEntry.get(),self.usernameEntry.get(),self.passwordEntry.get())
 
 			self.okButton.place_forget()
-			self.addB.place(relx=.8, rely=.1)
+			self.addB.place(relx=.7, rely=.1, anchor='center')
+			self.db.commit()
 
 
 	def entriesExist(self):
@@ -78,14 +87,51 @@ class Window(tk.Frame):
 			exists = True
 		return exists
 
-	def deleteEntry(url):
+	def deleteButton(self):
 		'''Remove website from database'''
+		self.deleteB = tk.Button(self.parent, text="Delete", height=30, width= 20, command=self.deleteButtonClicked)
+		if (self.entriesExist()):
+			self.deleteB.place(relx=.7, rely=.5, anchor='center')
 
-	def login():
+	def deleteButtonClicked(self):
+		'''Remove website from database. Give menu to enter nickname to be deleted.'''
+		deleteFrame = tk.Frame(self.parent, width=100, height=100)
+		deleteLabel = tk.Label(deleteFrame, text="Nickname: ")
+		deleteEntry = tk.Entry(deleteFrame)
+		deleteLabel.grid(row=0)
+		deleteEntry.grid(row=0, column= 1)
+		self.deleteB.place_forget()
+		deleteFrame.place(relx=.5,rely=.5, anchor='center')
+		self.okButton = tk.Button(self.parent, text="Done", command=self.deleteEntry(deleteEntry.get()))
+		self.okButton.place(relx=.5, rely=.7)
+		pass
+
+	def deleteEntry(self, nickname):
+		print('in')
+		self.db.execute("DELETE from accounts WHERE nickname=?;",(nickname,))
+		self.db.commit()
+		#self.okButton.place_forget()
+
+	def login(self,url,username,password):
 		'''Login to selected website'''
+		pass
+
+	def loadAccountButton(self, nickname, url, username, password):
+		'''Load buttons to screen based on inputed information'''
+		self.entries.append(tk.Button(self.accountButtonFrame, text=nickname, command=self.login(url,username,password)).grid(row=self.loadTimes))
+		self.loadTimes += 1
+		self.accountButtonFrame.place(relx=.5,rely=.5,anchor='center')
+
+
+	def loadEntries(self):
+		'''Load entries if there are any from database'''
+		cursor = self.db.execute("SELECT nickname, url, username, password from accounts;")
+		for row in cursor:
+			self.loadAccountButton(row[0], row[1], row[2], row[3])
+
 
 def main():
-	ui = Window()
 	root = tk.Tk()
+	ui = Window(root)
 	ui.create_window(root)
 	root.tk.mainloop()
